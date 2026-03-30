c2b2dcc9a851a5c715c622b4ab8887a174ea37f4
lalit1950 <132910982+lalit1950@users.noreply.github.com>
2026-03-30 07:13:38 +0530

    Refactor spark_config.py to simplify Spark session setup
    Removed unnecessary imports and JAVA_HOME configuration.

--- config/spark_config.py
+++ config/spark_config.py
@@ -1,15 +1,14 @@
-import os
-import findspark
+#import os
+#import findspark
 from pyspark.sql import SparkSession
-os.environ["JAVA_HOME"] = r"C:\Program Files\Java\jdk-11"
-findspark.init()  # This links your Python environment to local Spark
+#os.environ["JAVA_HOME"] = r"C:\Program Files\Java\jdk-11"
+#findspark.init()  # This links your Python environment to local Spark
 
 def get_spark_session(app_name: str = "MedallionPipeline"):
     spark= (SparkSession
         .builder
         .appName(app_name)
         .master("local[*]")
-        .config("spark.driver.memory", "4g")
         .getOrCreate()
     )
     return spark
